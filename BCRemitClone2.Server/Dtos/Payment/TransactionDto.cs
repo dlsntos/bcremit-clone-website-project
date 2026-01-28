f9349b17e44c08ba9dd057aaaaae083d102362eb
dean <deanlt.santos@gmail.com>
2026-01-29 04:14:34 +0800

    create TransactionDto file

---
+++ BCRemitClone2.Server/Dtos/Payment/TransactionDto.cs
@@ -0,0 +1,19 @@
+﻿using BCRemitClone2.Server.Enums;
+
+namespace BCRemitClone2.Server.Dtos.Payment
+{
+    public class TransactionDto
+    {
+        public Guid Id { get; set; }
+        public string UserId { get; set; } = default!;
+        public int BeneficiaryId { get; set; }
+        public decimal SendAmount { get; set; }
+        public decimal TransferFee { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string PurposeOfRemittance { get; set; } = string.Empty;
+        public string? PaymentMethod { get; set; } = default!;
+        public TransactionStatus Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? CompletedAt { get; set; }
+    }
+}
