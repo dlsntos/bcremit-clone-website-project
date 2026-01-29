d6a4aa8088993555dd315d5c830cbd32961b7ec1
dean <deanlt.santos@gmail.com>
2026-01-30 03:11:31 +0800

    create two modal files for success and failed transaction/payment

---
+++ bcremitclone2.client/src/features/send-money/components/modals/PaymentCancelledModal.tsx
@@ -0,0 +1,7 @@
+function PaymentCancelledModal() {
+  return (
+    <p>Hello world!</p>
+  );
+}
+
+export default PaymentCancelledModal;
---
+++ bcremitclone2.client/src/features/send-money/components/modals/PaymentSuccessModal.tsx
@@ -0,0 +1,7 @@
+function PaymentSuccessModal() {
+  return (
+    <p>Hello world!</p>
+  );
+}
+
+export default PaymentSuccessModal;
