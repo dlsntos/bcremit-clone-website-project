dd6e46c60e1abcaa15009dd1331e222d6779cca3
dean <deanlt.santos@gmail.com>
2026-01-30 05:13:23 +0800

    added new migration

--- BCRemitClone2.Server/Migrations/20260129211237_BankAccounts.Designer.cs
+++ BCRemitClone2.Server/Migrations/20260129211237_BankAccounts.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BCRemitClone2.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BCRemitClone2.Server.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20260129211237_BankAccounts")]
+    partial class BankAccounts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BCRemitClone2.Server/Migrations/20260129211237_BankAccounts.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BCRemitClone2.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class BankAccounts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e3eb10c5-123d-4396-a5d4-a53d36fc6244");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e714dc38-665a-4184-8be9-1f2d2c02f27e");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "3d6aa653-957c-4638-b4d9-53240fede420", null, "Admin", "ADMIN" },
+                    { "528103c0-0621-47cf-afe8-83e1dc9107a4", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3d6aa653-957c-4638-b4d9-53240fede420");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "528103c0-0621-47cf-afe8-83e1dc9107a4");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "e3eb10c5-123d-4396-a5d4-a53d36fc6244", null, "Admin", "ADMIN" },
+                    { "e714dc38-665a-4184-8be9-1f2d2c02f27e", null, "User", "USER" }
+                });
+        }
+    }
+}
