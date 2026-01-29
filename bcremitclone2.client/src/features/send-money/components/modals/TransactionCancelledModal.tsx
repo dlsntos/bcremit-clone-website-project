4e5f945146ea328345cce8733be3ace24f70f76f
dean <deanlt.santos@gmail.com>
2026-01-30 04:57:54 +0800

    implemented TransactionCancelledModal

--- bcremitclone2.client/src/features/send-money/components/modals/TransactionCancelledModal.tsx
+++ bcremitclone2.client/src/features/send-money/components/modals/TransactionCancelledModal.tsx
@@ -1,6 +1,30 @@
-function TransactionCancelledModal() {
+import type { ModalProps } from "../../../../types/modal";
+import Button from '../../../../components/ui/Button';
+import { useNavigate } from 'react-router';
+function TransactionCancelledModal({ successful = false }: ModalProps) {
+  const navigate = useNavigate();
+  const handleToDashboard = () => {
+    navigate("/dashboard");
+  }
   return (
-    <p>Hello world!</p>
+    <dialog
+      open={successful}
+      className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black/50 z-100"
+    >
+      <div className="flex flex-col justify-center items-center max-w-lg w-full h-[300px] py-10 bg-white text-center rounded-xl">
+        <section className="flex flex-row justify-center items-center">
+          <h1 className="text-bluewhale p-3 text-6xl font-bold font-figtree">Transaction Failed</h1>
+        </section>
+        <section className="w-2/3 ">
+          <Button
+            onClick={handleToDashboard}
+            className="block p-5 mt-5 w-full bg-red-600 text-white text-xl font-semibold rounded-xl shadow-sm transition duration-300 ease-in-out cursor-pointer hover:bg-gray-400"
+          >
+            Back to Dashboard
+          </Button>
+        </section>
+      </div>
+    </dialog>
   );
 }
 
