4f306acc623151e584ca7a32c012ebe6e94f9de0
dean <deanlt.santos@gmail.com>
2026-01-30 07:31:20 +0800

    add a prop for card image

--- bcremitclone2.client/src/components/ui/Card.tsx
+++ bcremitclone2.client/src/components/ui/Card.tsx
@@ -4,15 +4,17 @@ interface CardProps {
   description: string;
   buttonName: string;
   icon?: React.ReactNode;
+  cardImage?: React.ReactNode;
 }
 
-function Card({ title, description, buttonName, icon }: CardProps) {
+function Card({ title, description, buttonName, icon, cardImage }: CardProps) {
   return (
     <>
       <div
         className="flex flex-col p-5 w-full max-w-[400px] h-48 justify-between bg-white rounded-lg shadow-md cursor-pointer transition duration-300 hover:scale-110"
       >
-        <div>
+        <div className="flex flex-col">
+          <div>{cardImage}</div>
           <h1 className="text-bluewhale text-2xl font-bold">{title}</h1>
           <p className="text-md text-gray-500 mt-2"> {description} </p>
         </div>
