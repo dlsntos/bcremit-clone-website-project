82282cabebc124edf976cb8c54506e4b5a6f808a
dean <deanlt.santos@gmail.com>
2026-01-30 08:37:53 +0800

    add mobile responsiveness to Referral Card component

--- bcremitclone2.client/src/pages/dashboard/settings/components/ReferralCard.tsx
+++ bcremitclone2.client/src/pages/dashboard/settings/components/ReferralCard.tsx
@@ -4,16 +4,16 @@ function ReferralCard() {
   return (
     <div className="flex flex-row items-center p-5 mt-5 w-full max-w-md bg-blue-100 rounded-md transition duration-200 hover:scale-105">
       <div className="w-full">
-        <h2 className="text-3xl text-bluewhale font-bold">Invite a friend</h2>
-        <p className="text-bluewhale">Get your friend transact with BCRemit and earn referral rewards</p>
+        <h2 className="text-2xl sm:text-3xl text-bluewhale font-bold">Invite a friend</h2>
+        <p className="text-sm sm:text-md text-bluewhale">Get your friend transact with BCRemit and earn referral rewards</p>
         <Button
-          className="w-full p-3 mt-5 bg-blue-500 text-white rounded-md cursor-pointer"
+          className="w-full p-2 sm:p-3 mt-5 bg-blue-500 text-white rounded-md cursor-pointer"
         >
           Invite a friend
         </Button>
       </div>
       <div>
-        <img src={giftImage} className="h-30 w-50" />
+        <img src={giftImage} className="h-20 w-40 sm:h-30 sm:w-50" />
       </div>
     </div>
   );
