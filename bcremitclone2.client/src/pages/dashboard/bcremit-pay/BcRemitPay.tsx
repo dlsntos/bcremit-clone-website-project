d57afbd25c1a0d21e1d21f242d04abe56e042c99
dean <deanlt.santos@gmail.com>
2026-01-30 08:28:20 +0800

    integrated the bcremitPay card data and card component by mapping

--- bcremitclone2.client/src/pages/dashboard/bcremit-pay/BcRemitPay.tsx
+++ bcremitclone2.client/src/pages/dashboard/bcremit-pay/BcRemitPay.tsx
@@ -1,3 +1,5 @@
+import { bcRemitPayCardData } from "../../../data/cardData/bcremitPayCardData";
+import BcremitPayCard from "./components/BcremitPayCard";
 function BcRemitPay() {
   return (
     <div className="px-8">
@@ -5,8 +7,16 @@ function BcRemitPay() {
         <h1 className="mt-12 max-w-sm text-5xl font-extrabold text-bluewhale">BCRemit-Pay</h1>
         <p className="w-full max-w-sm text-xl text-gray-500">You can now pay your contributions and other payments using BCRemit-Pay!</p>
       </section>
-      <section>
-
+      <section className="w-full mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-[repeat(4,minmax(0,350px))] auto-rows-[11rem] md:auto-rows-[15rem] place-content-start gap-10">
+        {bcRemitPayCardData.map((cardData) => (
+          <BcremitPayCard
+            title={cardData.title!}
+            description={cardData.description!}
+            buttonName={cardData.title!}
+            icon={""}
+            cardImage={cardData.cardImage!}
+          />
+        ))}
       </section>
     </div>
   );
