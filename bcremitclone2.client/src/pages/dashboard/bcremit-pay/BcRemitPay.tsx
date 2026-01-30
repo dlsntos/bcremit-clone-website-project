506c78c5604cfcd5a1216d0bd5d15b24deae4a23
dean <deanlt.santos@gmail.com>
2026-01-30 08:30:40 +0800

    adjusted default row size (mobile endpoint)

--- bcremitclone2.client/src/pages/dashboard/bcremit-pay/BcRemitPay.tsx
+++ bcremitclone2.client/src/pages/dashboard/bcremit-pay/BcRemitPay.tsx
@@ -7,7 +7,7 @@ function BcRemitPay() {
         <h1 className="mt-12 max-w-sm text-5xl font-extrabold text-bluewhale">BCRemit-Pay</h1>
         <p className="w-full max-w-sm text-xl text-gray-500">You can now pay your contributions and other payments using BCRemit-Pay!</p>
       </section>
-      <section className="w-full mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-[repeat(4,minmax(0,350px))] auto-rows-[11rem] md:auto-rows-[15rem] place-content-start gap-10">
+      <section className="w-full mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-[repeat(4,minmax(0,350px))] auto-rows-[15rem] md:auto-rows-[15rem] place-content-start gap-10">
         {bcRemitPayCardData.map((cardData) => (
           <BcremitPayCard
             title={cardData.title!}
